988a5f6520d911ab20e58dc332b089535b2062b4
Tolegen777 <tolegenmukan@gmail.com>
2025-03-22 10:58:38 +0500

    fix icons

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,6 @@ const nextConfig: NextConfig = {
                 pathname: '/**',
             },
         ],
-    },
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack']
-        });
-        return config;
     }
 };
 
