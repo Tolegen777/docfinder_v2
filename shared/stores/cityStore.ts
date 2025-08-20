c7b660e47184bf8b1135ce636642249a1676e481
Tolegen777 <tolegenmukan@gmail.com>
2025-08-20 15:43:39 +0500

    Поменял урл бэкенда

--- shared/stores/cityStore.ts
+++ shared/stores/cityStore.ts
@@ -7,7 +7,10 @@ interface CityState {
 }
 
 export const useCityStore = create<CityState>((set, get) => ({
-    currentCity: null, // Default to first city (Алматы in most cases)
+    currentCity: {
+        title: 'Шымкент',
+        id: 3
+    },
 
     setCurrentCity: (city: City) => {
         set({ currentCity: city });
