1a59aef5362d212b1f51c14b7380256f8ec6824f
Tolegen777 <tolegenmukan@gmail.com>
2025-08-28 18:07:28 +0500

    вернул отзывы

--- app/doctor/[slug]/page.tsx
+++ app/doctor/[slug]/page.tsx
@@ -1,11 +1,11 @@
 import DoctorDetailPage from "@/components/DoctorDetail/DoctorDetailPage";
-// import {ReviewsList} from "@/shared/ui/Reviews/ReviewsList";
+import {ReviewsList} from "@/shared/ui/Reviews/ReviewsList";
 
 export default function Doctor() {
     return (
         <div>
             <DoctorDetailPage />
-            {/*<ReviewsList type="doctor" showClinicLink={true} />*/}
+            <ReviewsList type="doctor" showClinicLink={true} />
         </div>
     );
 }
