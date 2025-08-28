9b30a4a3e30648009e3d8730248056c4f0e1886f
Tolegen777 <tolegenmukan@gmail.com>
2025-08-28 10:59:51 +0500

    убрал отзывы

--- app/clinic/[slug]/page.tsx
+++ app/clinic/[slug]/page.tsx
@@ -1,13 +1,13 @@
 import ClinicDetailsBlock from "@/components/ClinicDetails/ClinicDetailBlock/ClinicDetailsBlock";
 import {ClinicDoctorsList} from "@/components/ClinicDetails/ClinicDoctorsList/ClinicDoctorsList";
-import {ReviewsList} from "@/shared/ui/Reviews/ReviewsList";
+// import {ReviewsList} from "@/shared/ui/Reviews/ReviewsList";
 
 export default function Clinic() {
     return (
         <div>
             <ClinicDetailsBlock/>
             <ClinicDoctorsList/>
-            <ReviewsList type="clinic" showClinicLink={false} />
+            {/*<ReviewsList type="clinic" showClinicLink={false} />*/}
         </div>
     );
 }
